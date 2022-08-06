81de0b237308a146e55766ea269276d03f7edbd4
mike yang <18604713262@163.com>
2022-08-06 00:14:36 -0400

    Android support for native functions (Java)

---
+++ android/app/src/main/java/com/pdmnotes/PdmNativeCryptModule.java
@@ -0,0 +1,32 @@
+package com.pdmnotes;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import java.util.Map;
+import java.util.HashMap;
+
+public class PdmNativeCryptModule extends ReactContextBaseJavaModule {
+    PdmNativeCryptModule(ReactApplicationContext context) {
+        super(context);
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "PdmNativeCryptModule";
+    }
+    @ReactMethod
+    public void echoer(String input , Callback callback ) {
+        Log.d("CryptModule", "echoer called with name: " + getName()
+                + " and location: " + input);
+        System.out.printf("Java echoer %s \n",input);
+        callback.invoke(input);
+    }
+}
--- components/views/SettingPage.tsx
+++ components/views/SettingPage.tsx
@@ -1,11 +1,28 @@
-import {Button, Text, View} from "react-native";
+import { Text, View} from "react-native";
 import {styles} from "../../assets/Style";
+import { NativeModules, Button } from 'react-native';
 import React from "react";
 
 export default function SettingsScreen({ navigation }) {
-    return (
-        <View style={styles.mview}>
-            <Text style={styles.somet}>Settings Screen</Text>
-        </View>
+  const [nativeReturn,onNativeReturn]=React.useState('Nothing');
+  const { PdmNativeCryptModule } = NativeModules;
+
+  const onPress = () => {
+    let nReturn;
+    PdmNativeCryptModule.echoer(
+      "This from react native!!!",
+      (back)=>{
+        onNativeReturn(back)
+      }
     );
+  };
+  return (
+      <View style={styles.mview}>
+          <Text >{nativeReturn}</Text>
+          <Button
+            title={"Test Native modules(Java)."}
+            onPress={onPress}
+          ></Button>
+      </View>
+  );
 }
