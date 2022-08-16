8634a700caf929ba46d748483a114520833168d9
mike yang <18604713262@163.com>
2022-08-16 14:41:46 -0400

    Network fixed for all platforms.

--- components/handle/network/net.tsx
+++ components/handle/network/net.tsx
@@ -3,19 +3,21 @@ import { Component } from "react";
 export default class Net {
   static async post(url:string, arg1: any, arg2:any=null){
     try {
-      const response = await fetch(
-        url,
-        {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: arg1
-        }
-      );
-      const json = await response.json();
-      return json.movies;
+      // const response = await
+        return fetch(
+          url,
+          {
+            method: 'POST',
+            body: arg1
+          }
+        );
+        //   .then(function(response){
+        //   return response.json();
+        // }).then(function(data){
+        //   console.log(`Net return1: ${data.umail}`)
+        //   console.log(`Net return2: ${JSON.stringify(data)}`)
+        //   return data;
+        // });
     } catch (error) {
       console.error(error);
       return null;
--- components/handle/network/netCalls.tsx
+++ components/handle/network/netCalls.tsx
@@ -3,11 +3,14 @@ import Net from "./net";
 export default class NetCalls {
   private static signinURL: string='https://pdm.pw/auth/signin';
   static async signin(umail:any, upw:any){
-    let httpOut= await Net.post(this.signinURL, JSON.stringify({ "umail": umail, "upw": upw }));
-    // const data = await httpOut.json();
-    // console.log(data);
-    httpOut.then(data=>{
-      console.log(`httpOut: ${data}`);
-    });
+    Net.post(this.signinURL, JSON.stringify({ "umail": umail, "upw": upw }))
+      .then(function(res){
+        return res.json();
+      })
+      .then(function(data){
+          console.log(`http return1: ${data.umail}`)
+          console.log(`http return2: ${JSON.stringify(data)}`)
+          return data;
+        });
   }
 }
