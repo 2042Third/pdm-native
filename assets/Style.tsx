99fe5acdd0d8a5a2551b65c96a4544125d67a3fb
mike yang <18604713262@163.com>
2022-08-31 12:03:44 -0400

    note menu touchable

--- assets/Style.tsx
+++ assets/Style.tsx
@@ -25,6 +25,49 @@ export const colors = {
 };
 
 export const styles = StyleSheet.create({
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: colors['--background-default'],
+    borderRadius: 7,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 7,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+
   // TEST END
   // GENERAL STYLES
   shiftContainer: {
--- components/views/Notes/NotesMenu.tsx
+++ components/views/Notes/NotesMenu.tsx
@@ -1,7 +1,7 @@
 import { oneOfType } from "prop-types";
-import React, { useEffect } from "react";
-import { FlatList, RefreshControl, SafeAreaView, Text } from "react-native";
-import { View } from "react-native-ui-lib";
+import React, { useCallback, useEffect } from "react";
+import {  FlatList, Modal, Pressable, RefreshControl, SafeAreaView, Text } from "react-native";
+import { ActionSheet, View } from "react-native-ui-lib";
 import { shallowEqual, useSelector } from "react-redux";
 import { styles } from "../../../assets/Style";
 import { useAppDispatch, useAppSelector } from "../../handle/redux/hooks";
@@ -25,10 +25,42 @@ const NotesMenu = ({  }) => {
   const noteids = useSelector(selectNoteId, shallowEqual);
 
   const [refreshing, setRefreshing] = React.useState(false);
+  const [noteOptionsMenu, setNoteOptionsMenu] = React.useState(false);
+
+  useEffect(() => {
+    getHeadsFromServer();
+  }, [user]);
+
+  useEffect(() => {
+    console.log("mountin note menu");
+  }, []);
 
   const onRefresh = ()=>{
     getHeadsFromServer();
   }
+
+  const onSelectNote = (key: string) => {
+    navigation.navigate("NotesEdit", {});
+    const selectedHead = selectNoteByKey(noteHead, key);
+    console.log(JSON.stringify(selectedHead));
+    dispatch(getNote({ user: user, note_id: selectedHead.note_id }));
+  };
+
+  const onLongPressNote = (key: string) => {
+    console.log(`Long press note item ${key}`);
+    setNoteOptionsMenu(true);
+  };
+
+  const createNewNote = () => {
+    if (userinfo.status === 'success') {
+      console.log(`dispatched new note`);
+      dispatch(newNote({ user: user, noteMsg: new NotesMsg }));
+    }
+  };
+
+  const pickNoteOption = (option:string) => {
+    console.log("picked note option "+option);
+  };
 
   const getHeadsFromServer = () => {
     if (user.sess !== '') {
@@ -36,62 +68,120 @@ const NotesMenu = ({  }) => {
       if (userinfo.status === "success") {
         setRefreshing(true);
         dispatch(getHeads({ userinfo: userinfo, user: user }))
-        .then(()=>{
-          setRefreshing(false);
-        })
-        .catch((e) => { 
-          setRefreshing(false); 
-          console.log("Failue getting heads from server:\n"+e)
-        });
+          .then(() => {
+            setRefreshing(false);
+          })
+          .catch((e) => {
+            setRefreshing(false);
+            console.log("Failue getting heads from server:\n" + e)
+          });
       }
     }
   }
 
-  useEffect(() => {
-    getHeadsFromServer();
-  }, [user]);
+//   class NoteItemCell extends React.PureComponent {
+  
+//   item = this.props.item;
+//   render(){
+//     const itemObj = selectNoteByKey(noteHead, this.item);
+//     console.log("item mounting => " + JSON.stringify(this.item));
+//     return (
+//       <View
+//         key={itemObj.key}
+//         style={[{ flexDirection: "row", justifyContent: 'space-between' }]}
+//         >
 
-  useEffect(()=>{
-    console.log("mountin note menu");
-  },[]);
+//         <TouchableOpacity
+//           style={[{}]}
+//           onPress={() => { onSelectNote(itemObj.key) }}
 
+//         >
+//           {/* <View style={[styles.notesListingItemContainer]}> */}
+//           <Text style={[styles.normalText]}
+//           >
+//             {itemObj.head === '' ? 'Unnamed note ' + itemObj.id : itemObj.head}
+//           </Text>
+//           <Text style={[styles.smallText]}>
+//             {itemObj.ctime}
+//           </Text>
+//           {/* </View> */}
+//         </TouchableOpacity>
 
-  const onSelectNote = (key:string)=> {
-    navigation.navigate("NotesEdit",{});
-    const selectedHead = selectNoteByKey(noteHead,key);
-    console.log(JSON.stringify(selectedHead));
-    dispatch(getNote({user:user,note_id:selectedHead.note_id}));
-  };
-
-  const NoteItemCell = ({item, ...props}) => {
+//         <TouchableOpacity
+//           style={[{ width: 40, }]}
+//           onPress={() => { onLongPressNote(itemObj.key) }}
+//         >
+//           <Icon style={[styles.menuButton]}
+//             name={'dots-vertical'} size={30}
+//           />
+//         </TouchableOpacity>
+//       </View>
+//     );
+//   }
+// } 
+  const _renderItem = ({ item }) => (
+    // React.memo(NoteItemCell(item ))
+    <NoteItemCell item={item}/>
+  );
+  const NoteItemCell = ({item}) => {
     console.log("item mounting => " + JSON.stringify(item));
 
     const itemObj = selectNoteByKey(noteHead, item);
     return (
-      <TouchableOpacity
+      <View 
         key={itemObj.key}
-        onPress={() => { onSelectNote(itemObj.key) }}
-      >
-        <View style={[styles.notesListingItemContainer]}>
-          <Text style={[styles.normalText]}>{itemObj.head === '' ? 'Unnamed note ' + itemObj.id : itemObj.head}</Text>
-          <Text style={[styles.smallText]}> {itemObj.ctime}</Text>
-        </View>
-      </TouchableOpacity>
-      
-    );
-  } 
+        style={[{  flexDirection: "row", justifyContent: 'space-between' }]}>
 
-  
-  const createNewNote = () => {
-    if(userinfo.status === 'success'){
-      console.log(`dispatched new note`);
-      dispatch(newNote({ user: user, noteMsg: new NotesMsg }));
-    }
-  };
+        <TouchableOpacity
+          style={[{ }]}
+          onPress={() => { onSelectNote(itemObj.key) }}
+        >
+          <View style={[styles.notesListingItemContainer]}
+          // onPress={({itemObj}) =>  onSelectNote(itemObj.key) }
+          >
+            <Text style={[styles.normalText]}
+            >
+              {itemObj.head === '' ? 'Unnamed note ' + itemObj.id : itemObj.head}
+            </Text>
+            <Text style={[styles.smallText]}> 
+              {itemObj.ctime}
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[{  width: 40,  }]}
+          onPress={() => { onLongPressNote(itemObj.key) }}
+        >
+          <Icon style={[styles.menuButton]}
+            name={'dots-vertical'} size={30}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  const noteItemKeyExtractor = useCallback((item) => item,[]);
 
   return (
     <View style={[ styles.noteMenuViewStyle]}>
+      
       <View style={styles.noteMenuContent}>
+        <ActionSheet
+          title={'Title'}
+          message={'Message of action sheet'}
+          cancelButtonIndex={3}
+          destructiveButtonIndex={0}
+          options={[
+            { label: 'option 1', onPress: () => pickNoteOption('option 1') },
+            { label: 'option 2', onPress: () => pickNoteOption('option 2') },
+            { label: 'option 3', onPress: () => pickNoteOption('option 3') },
+            { label: 'cancel', onPress: () => pickNoteOption('cancel') }
+          ]}
+          visible={noteOptionsMenu}
+          useNativeIOS
+          onDismiss={() => setNoteOptionsMenu( false )}
+        />
         <TouchableOpacity
           style={[
             styles.lightContainerColor,
@@ -105,19 +195,24 @@ const NotesMenu = ({  }) => {
           <Text style={[styles.normalText]}>New Note</Text>
         </TouchableOpacity>
         <View
-          style={[{}]}
+          style={[{height:600}]}
         >
           <FlatList
             data={noteids}
-            renderItem={({ item }) => <NoteItemCell item={item} />}
+            renderItem={NoteItemCell }
+            maxToRenderPerBatch={6}
+            windowSize={2}
+            initialNumToRender={12}
+            // renderItem={({ item }) => <NoteItemCell item={item} />}
             refreshControl={
               <RefreshControl
                 refreshing={refreshing}
                 onRefresh={onRefresh}
               />
             }
-          // keyExtractor={item => item.key}
-          // extractData={}
+            removeClippedSubviews={true}
+            keyExtractor={noteItemKeyExtractor}
+            extraData={noteids}
           >
           </FlatList>
         </View>
@@ -128,3 +223,7 @@ const NotesMenu = ({  }) => {
 }
 // export default NotesMenu;
 export default React.memo(NotesMenu);
+function render() {
+  throw new Error("Function not implemented.");
+}
+
