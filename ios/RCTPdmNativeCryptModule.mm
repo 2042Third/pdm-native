2c05ed211d94be5606b3bb1c347b2f436abd4a4b
mike yang <18604713262@163.com>
2022-09-29 01:29:46 -0400

    Added Persistent User Setting for number of times app password can be entered.

--- ios/RCTPdmNativeCryptModule.mm
+++ ios/RCTPdmNativeCryptModule.mm
@@ -13,39 +13,69 @@ RCT_EXPORT_MODULE(PdmNativeCryptModule);
 {
   return NO;
 }
-RCT_EXPORT_METHOD(echoer:( NSString*)a  callback: (RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(
+        echoer:( NSString*)a
+        callback: (RCTResponseSenderBlock)callback
+        resolver:(RCTPromiseResolveBlock)resolve
+        rejecter:(RCTPromiseRejectBlock)reject
+    )
 {
   RCTLogInfo(@"Pretending to create an event (this is from a objective-c++ module) %@", a);
+  resolve(@[@"from Objective-C++ .mm"]);
 
-  
-  callback(@[@"from Objective-C++ .mm"]);
+//       callback(@[@"from Objective-C++ .mm"]);
 }
 
-RCT_EXPORT_METHOD(getHash:( NSString*)a  callback: (RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(
+getHash:( NSString*)a
+resolver:(RCTPromiseResolveBlock)resolve
+rejecter:(RCTPromiseRejectBlock)reject
+)
+
 {
   std::string input_a = std::string([a UTF8String]);
   std::string out_b = get_hash(input_a);
   NSString *b = [NSString stringWithCString:out_b.c_str()
                                      encoding:[NSString defaultCStringEncoding]];
-  callback(@[b]);
+  resolve(@[b]);
 }
-RCT_EXPORT_METHOD(enc:( NSString*)a data:(NSString*)b callback: (RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(
+enc:( NSString*)a
+data:(NSString*)b
+resolver:(RCTPromiseResolveBlock)resolve
+rejecter:(RCTPromiseRejectBlock)reject
 {
   std::string input_a = std::string([a UTF8String]);
   std::string input_b = std::string([b UTF8String]);
   std::string out_b = loader_check( input_a, input_b);
   NSString *out_a = [NSString stringWithCString:out_b.c_str()
                                      encoding:[NSString defaultCStringEncoding]];
-  callback(@[out_a]);
+
+    if (out_a){
+      resolve(@[out_a]);
+    }
+    else {
+      reject(@[@"Encryption failure"]);
+    }
 }
-RCT_EXPORT_METHOD(dec:( NSString*)a data:(NSString*)b callback: (RCTResponseSenderBlock)callback)
+
+RCT_EXPORT_METHOD(
+dec:( NSString*)a
+data:(NSString*)b
+resolver:(RCTPromiseResolveBlock)resolve
+rejecter:(RCTPromiseRejectBlock)reject
 {
   std::string input_a = std::string([a UTF8String]);
   std::string input_b = std::string([b UTF8String]);
   std::string out_b = loader_out( input_a, input_b);
   NSString *out_a = [NSString stringWithCString:out_b.c_str()
                                      encoding:[NSString defaultCStringEncoding]];
-  callback(@[out_a]);
+  if (out_a){
+    resolve(@[out_a]);
+  }
+  else {
+    reject(@[@"Decryption failure, password incorrect"]);
+  }
 }
 
 @end
