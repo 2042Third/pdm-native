4172054c744db588413837f2053bec24434c0b25
mike yang <18604713262@163.com>
2022-10-09 16:26:12 -0400

    android get hash and user native modules fixed.

--- components/handle/handlers/user.tsx
+++ components/handle/handlers/user.tsx
@@ -1,113 +1,113 @@
 import { NativeModules } from "react-native";
 import { useSelector } from "react-redux";
 
-// const { PdmNativeCryptModule } = NativeModules;
-// export default PdmNativeCryptModule;
-//
-// export const echoer = async (a:string) => {
-//   const { PdmNativeCryptModule } = NativeModules;
-//   return PdmNativeCryptModule.echoer(a);
-// }
-//
-// export const getHash = async (a:string) => {
-//   const { PdmNativeCryptModule } = NativeModules;
-//   return PdmNativeCryptModule.getHash(a);
-// }
-// export const dec = async (upw: string, a: string) => {
-//   const { PdmNativeCryptModule } = NativeModules;
-//   // console.log(`dec called with ps: ${upw}`);
-//   try {
-//     const out: string = PdmNativeCryptModule.dec(upw, a);
-//     return out;
-//   }
-//   catch (e) {
-//     console.log(e);
-//     return null;
-//   }
-// }
-//
-// /**
-//  * Same as getHash, but return a promise,
-//  *
-// */
-// export const makeHash = async (a: string) => {
-//   const { PdmNativeCryptModule } = NativeModules;
-//   // console.log(`dec called with ps: ${upw}`);
-//   try {
-//     const out: string = PdmNativeCryptModule.getHash( a);
-//     return out;
-//   }
-//   catch (e) {
-//     console.log(e);
-//     return null;
-//   }
-// }
-//
-// export const enc = async (upw: string, plain: string) => {
-//   const { PdmNativeCryptModule } = NativeModules;
-//   // return PdmNativeCryptModule.enc(upw, plain, (a: string) => {
-//   //   return a;
-//   // });
-//   try {
-//     const out: string = PdmNativeCryptModule.enc(upw, plain);
-//     return out;
-//   }
-//   catch (e) {
-//     console.log(e);
-//     return null;
-//   }
-// }
+const { PdmNativeCryptModule } = NativeModules;
+export default PdmNativeCryptModule;
 
- // export class cryptModule {
- //    private PdmNativeCryptModule:any;
- //    constructor () {
- //      let { PdmNativeCryptModule } = NativeModules;
- //      this.PdmNativeCryptModule = PdmNativeCryptModule;
- //    }
- //
- //    async echoer (a:string){
- //      try{
- //        const out: string = this.PdmNativeCryptModule.echoer(a);
- //        return out;
- //      }
- //      catch (e) {
- //        console.log(e);
- //        return null;
- //      }
- //    }
- //
- //
- //    async getHash(a:string){
- //      try {
- //        const out: string = this.PdmNativeCryptModule.getHash(a);
- //        return out;
- //      }
- //      catch (e) {
- //        console.log(e);
- //        return null;
- //      }
- //    }
- //
- //    async enc(upw: string, plain: string) {
- //      try {
- //        const out: string = this.PdmNativeCryptModule.enc(upw, plain);
- //        return out;
- //      }
- //      catch (e) {
- //        console.log(e);
- //        return null;
- //      }
- //    }
- //
- //   async dec (upw: string, a: string) {
- //     try {
- //       const out: string = this.PdmNativeCryptModule.dec(upw, a);
- //       return out;
- //     }
- //     catch (e) {
- //       console.log(e);
- //       return null;
- //     }
- //   }
- //  }
+export const echoer = async (a:string) => {
+  const { PdmNativeCryptModule } = NativeModules;
+  return PdmNativeCryptModule.echoer(a);
+}
+
+export const getHash = async (a:string) => {
+  const { PdmNativeCryptModule } = NativeModules;
+  return PdmNativeCryptModule.getHash(a);
+}
+export const dec = async (upw: string, a: string) => {
+  const { PdmNativeCryptModule } = NativeModules;
+  // console.log(`dec called with ps: ${upw}`);
+  try {
+    const out: string = PdmNativeCryptModule.dec(upw, a);
+    return out;
+  }
+  catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
+/**
+ * Same as getHash, but return a promise,
+ *
+*/
+export const makeHash = async (a: string) => {
+  const { PdmNativeCryptModule } = NativeModules;
+  // console.log(`dec called with ps: ${upw}`);
+  try {
+    const out: string = PdmNativeCryptModule.getHash( a);
+    return out;
+  }
+  catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
+export const enc = async (upw: string, plain: string) => {
+  const { PdmNativeCryptModule } = NativeModules;
+  // return PdmNativeCryptModule.enc(upw, plain, (a: string) => {
+  //   return a;
+  // });
+  try {
+    const out: string = PdmNativeCryptModule.enc(upw, plain);
+    return out;
+  }
+  catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
+ export class cryptModule {
+    private PdmNativeCryptModule:any;
+    constructor () {
+      let { PdmNativeCryptModule } = NativeModules;
+      this.PdmNativeCryptModule = PdmNativeCryptModule;
+    }
+
+    async echoer (a:string){
+      try{
+        const out: string = this.PdmNativeCryptModule.echoer(a);
+        return out;
+      }
+      catch (e) {
+        console.log(e);
+        return null;
+      }
+    }
+
+
+    async getHash(a:string){
+      try {
+        const out: string = this.PdmNativeCryptModule.getHash(a);
+        return out;
+      }
+      catch (e) {
+        console.log(e);
+        return null;
+      }
+    }
+
+    async enc(upw: string, plain: string) {
+      try {
+        const out: string = this.PdmNativeCryptModule.enc(upw, plain);
+        return out;
+      }
+      catch (e) {
+        console.log(e);
+        return null;
+      }
+    }
+
+   async dec (upw: string, a: string) {
+     try {
+       const out: string = this.PdmNativeCryptModule.dec(upw, a);
+       return out;
+     }
+     catch (e) {
+       console.log(e);
+       return null;
+     }
+   }
+  }
 
