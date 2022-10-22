c7728e7f1f977fc6002bd4956d59f379cf7c919f
mike yang <18604713262@163.com>
2022-10-22 18:01:21 -0400

    Sorting option menu in notes menu

--- components/handle/redux/selectors/selectorNoteHeads.tsx
+++ components/handle/redux/selectors/selectorNoteHeads.tsx
@@ -1,16 +1,8 @@
 import { createSelector } from "reselect";
 import React, { useCallback, useEffect } from "react";
 import { NoteHead, NotesMenu } from "../../types";
-import { createStoreHook, shallowEqual, useSelector } from "react-redux";
-import { useAppSelector } from "../hooks";
-// @ts-ignore
-import get from "lodash/get";
-// @ts-ignore
-import orderBy from "lodash/orderBy";
 import { NoteSortingTypes } from "../reducers/notes/notesMenuReducer";
-import { selectNoteByKey } from "../reducers/notes/notesHeadsReducer";
 import { RootState } from "../store";
-import notesMenu from "../../../views/Notes/NotesMenu";
 
 export const selectNoteId = ( state:RootState)  => state.noteHeads;
 export const noteMenuOptions =( state:RootState) => state.notesMenu.sortingBy;
